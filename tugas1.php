aa802a5f6994b37670301a889075815fafb882b5
Decky Fajar Sidiq <46802206+deckyfajars@users.noreply.github.com>
2021-07-06 17:22:23 +0700

    Add files via upload

---
+++ tugas1.php
@@ -0,0 +1,91 @@
+<?php
+  $ayam_goreng = 13000;
+  $ayam_bakar = 15000;
+  $ayam_sayur = 13000;
+  $tempe_goreng = 1000;
+  $tahu_goreng = 1000;
+  $soto  = 3000;
+  $nasi_putih = 5000;
+  $es_teh = 4000;
+  $es_tebu = 5000;
+  echo "<h2>Daftar Menu</h2>
+  <table>
+  <ul>
+    <tr>
+      <td><li>Ayam Goreng</li></td>
+      <td>= Rp. $ayam_goreng</td>
+    </tr>
+    <tr>
+      <td><li> Ayam Bakar</li></td>
+      <td>= Rp. $ayam_bakar</td>
+    </tr>
+    <tr>
+      <td><li> Ayam Sayur</li></td>
+      <td>= Rp. $ayam_sayur</td>
+    </tr>
+    <tr>
+      <td><li> Tempe Goreng</li></td>
+      <td>= Rp. $tempe_goreng</td>
+    </tr>
+    <tr>
+      <td><li> Tahu Goreng</li></td>
+      <td>= Rp. $tahu_goreng</td>
+    </tr>
+    <tr>
+      <td><li> Soto</li></td>
+      <td>= Rp. $soto</td>
+    </tr>
+    <tr>
+      <td><li> Nasi Putih</li></td>
+      <td>= Rp. $nasi_putih</td>
+    </tr>
+    <tr>
+      <td><li> Es Teh</li></td>
+      <td>= Rp. $es_teh</td>
+    </tr>
+    <tr>
+      <td><li> Es Tebu</li></td>
+      <td>= Rp. $es_tebu</td>
+    </tr>
+  </ul>
+  </table>
+
+  <br>======================<br>
+
+  <h2>DAFTAR MENU</h2>
+  <table border=1>
+    <tr>
+      <th>No. </th>
+      <th>Nama Menu </th>
+      <th>Harga </th>
+    </tr>
+    <tr>
+      <td>1</td>
+      <td>Cireng</td>
+      <td>Rp. 1000</td>
+    </tr>
+    <tr>
+      <td>2</td>
+      <td>Es Duren</td>
+      <td>Rp. 15000</td>
+    </tr>
+    <tr>
+      <td>3</td>
+      <td>Es Milo</td>
+      <td>Rp. 20000</td>
+    </tr>
+    <tr>
+      <td>4</td>
+      <td>Es Teh</td>
+      <td>Rp. 5000</td>
+    </tr>
+    <tr>
+      <td>5</td>
+      <td>Piscok</td>
+      <td>Rp. 1000</td>
+    </tr>
+  </table>
+  ";
+
+
+?>
